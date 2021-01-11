84f71317e0676da9022873c6ec891e2162681443
gozo777 <myinvesthouse@gmail.com>
2021-01-11 13:09:37 +0100

    first commit

---
+++ src/components/pages/Login.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Login = () => {
+  return (
+    <div>
+      <h1>Логин</h1>
+    </div>
+  )
+}
+
+
---
+++ src/components/pages/Map.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Map = () => {
+  return (
+    <div>
+      <h1>Карта</h1>
+    </div>
+  )
+}
+
